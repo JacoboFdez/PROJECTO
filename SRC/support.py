44fd264d68921874c2f595646dfe1b2488a52a18
JacoboFdez <jacobochamadoira@gmail.com>
2023-01-23 17:36:25 +0100

    333

--- SRC/support.py
+++ SRC/support.py
@@ -1,24 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[2]:
-
-
-import pandas as pd
+import sys 
+sys.path.append("../")
+import src.support as sp 
+import pandas as pd 
 import numpy as np
 
 
-# In[4]:
+
 
 
 def porcentaje_nulos(a):
     return (a.isnull().sum() / a.shape[0] * 100).reset_index()
 
-
-# In[5]:
-
-
 def borrar_duplicados(a):
     return (a).drop_duplicates()
-    
-
